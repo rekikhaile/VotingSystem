0f5e6a28d781f7415c76b820f3dcdfe6e1f61f33
Riri <rekikhaile@gmail.com>
2019-01-29 15:11:20 -0500

    starting voting

---
+++ src/main/java/com/riri/votingsystem/entity/Citizen.java
@@ -0,0 +1,38 @@
+package com.riri.votingsystem.entity;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name="Citizens")
+public class Citizen {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name="citizen_name")
+	private String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+
+}
